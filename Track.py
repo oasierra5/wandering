f31d711c71f2ef6e009c656877a89b43d944a93e
Juanortiz502 <91995357+Juanortiz502@users.noreply.github.com>
2022-02-15 14:59:16 -0500

    Creamos las clases de wnadering y track para el proyecto

---
+++ Track.py
@@ -0,0 +1,15 @@
+from msilib.schema import Class
+
+
+class Track:
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def move(self, delta_x, delta_y):
+        return Track(self.x + delta_x, self.y + delta_y)
+    
+    def distancie(self, other_Track):
+        delta_x = self.x -other_Track.x
+        delta_y = self.y -other_Track.y
